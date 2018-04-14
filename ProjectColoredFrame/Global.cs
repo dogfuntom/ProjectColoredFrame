f376c63830665f8b077ed259da8495021f84aa67
Maxim Kamalov <maximkamalov@outlook.com>
2018-04-14 20:05:49 +0300

    updated version number and release notes

--- ProjectColoredFrame/Global.cs
+++ ProjectColoredFrame/Global.cs
@@ -5,18 +5,16 @@
     using ProjectColoredFrame.Core;
 
     /// <summary>
-    /// Inevitably every project has at least a few full-blown globals.
+    /// Inevitably every project has at least a few all-out globals.
     /// Instead of keeping them hidden or disguised as something minor,
     /// why not expose and list them in one place here.
     /// </summary>
-    /// <remarks>
-    /// Here full-blown global means something less innocent than Color.Green or string.Empty,
-    /// i.e. something closer to MyFavoriteSingletonService.Instance.
-    /// </remarks>
     internal static class Global
     {
-        public const string Name = "ProjectColoredFrame";
-        public const string OptionsPageName = "General";
+#pragma warning disable CC0021 // Use nameof. It's used in persistance, better not change it ever, but namespace can be renamed freely.
+		public const string Name = "ProjectColoredFrame";
+#pragma warning restore CC0021 // Use nameof
+		public const string OptionsPageName = "General";
 
         private static DTE2 DTE => (DTE2)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(DTE));
 
